1647c7d57ec4318fb5915e7d3cdbb38b0966dd93
kidd1118 <kidd1118@users.noreply.github.com>
2016-06-08 19:09:45 +0800

    Create ap.dom.js

---
+++ js/ap/ap.dom.js
@@ -0,0 +1,20 @@
+ap.dom = function () { };
+ap.dom.prototype = {
+    createElement: function (type, className, text, appendTo) {
+        var dom = document.createElement(type);
+        if (className != null && className != undefined) dom.className = className;
+        if (text != null && text != undefined) dom.textContent = dom.innerText = text;
+        if (appendTo != null && appendTo != undefined) appendTo.appendChild(dom);
+        return dom;
+    },
+    addEventListener: function (evnt, elem, func) {
+        if (elem.addEventListener)
+            elem.addEventListener(evnt, func, false);
+        else if (elem.attachEvent) {
+            elem.attachEvent("on" + evnt, func);
+        } else {
+            elem[evnt] = func;
+        }
+    }
+}
+var dom = new ap.dom();
