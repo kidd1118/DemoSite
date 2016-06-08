89d20c4e91486ff0894d9aa07545fac1aca35d93
kidd1118 <kidd1118@users.noreply.github.com>
2016-06-08 19:15:47 +0800

    Update ap.dom.js

--- js/ap/ap.dom.js
+++ js/ap/ap.dom.js
@@ -3,7 +3,7 @@ ap.dom.prototype = {
     createElement: function (type, className, text, appendTo) {
         var dom = document.createElement(type);
         if (className != null && className != undefined) dom.className = className;
-        if (text != null && text != undefined) dom.textContent = dom.innerText = text;
+        if (text != null && text != undefined) dom.textContent = dom.innerText = dom.value = text;
         if (appendTo != null && appendTo != undefined) appendTo.appendChild(dom);
         return dom;
     },
